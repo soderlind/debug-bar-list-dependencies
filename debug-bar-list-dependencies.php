6f24069f24928ac412fb060a8fd0d210048648ab
soderlind <per@soderlind.no>
2013-05-08 20:52:57 +0200

    Initial version

---
+++ debug-bar-list-dependencies.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name:  Debug Bar List Script & Style Dependencies
+Version: 1.0
+Plugin URI: http://soderlind.no/list-script-an…e-dependencies/ ‎
+Description:
+Author: Per Soderlind
+Author URI: http://www.soderlind.no
+*/
+
+
+function ps_listdeps_debug_bar_panels( $a ) {
+	if ( class_exists( 'Debug_Bar_Panel' ) ) {
+
+		class ps_listdeps_Debug_Bar_Panel extends Debug_Bar_Panel {
+
+			function render( ) {
+				global $wp_scripts, $wp_styles;
+?>
+			<div class="xx">
+
+				<table width="100%" cellspacing="2" cellpadding="5" class="form-table">
+					<tr><th colspan="3" style="font-size: 1.5em;font-weight: bold">Enqueued Scripts</th></tr>
+					<tr><td>Order</td><td><b>Loaded</b></td><td><b>Dependencies</b></td></tr>
+					<?php
+				$i = 1;
+				foreach ( $wp_scripts->print_scripts() as $loaded_scripts ) {
+
+					echo '<tr style="background-color:',  ( $i % 2 === 0 ) ? '#eee' : '#fff' , '"><td>', $i, '<td>', $loaded_scripts, '</td><td>', ( count( $wp_scripts->registered[$loaded_scripts]->deps ) > 0 ) ?  implode( " and ", $wp_scripts->registered[$loaded_scripts]->deps ) : '', '</td></tr>', "\n";
+					$i++;
+				}
+?>
+					<tr><th colspan="3" style="font-size: 1.5em;font-weight: bold">Enqueued Styles</th></tr>
+					<tr><td>Order</td><td><b>Loaded</b></td><td><b>Dependencies</b></td></tr>
+					<?php
+				$i = 1;
+				foreach ( $wp_styles->registered as $handle => $loaded_styles ) {
+					echo '<tr style="background-color:',  ( $i % 2 === 0 ) ? '#eee' : '#fff' , '"><td>', $i, '<td>', $handle, '</td><td>', ( count( $wp_styles->registered[$handle]->deps ) > 0 ) ?  implode( " and ", $wp_styles->registered[$handle]->deps ) : '', '</td></tr>', "\n";
+					$i++;
+				}
+?>
+				</table>
+			</div>
+			<?php
+			}
+		}
+
+
+		$a[]=new ps_listdeps_Debug_Bar_Panel( 'Script & Style Dependencies' );
+	}
+	return $a;
+}
+add_filter( 'debug_bar_panels', 'ps_listdeps_debug_bar_panels' );
