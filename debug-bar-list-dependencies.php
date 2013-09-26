56628130e123f7f26837e116d0aabf5b6d6ade4c
soderlind <per@soderlind.no>
2013-09-26 12:32:34 +0200

    Fixed https://github.com/soderlind/debug-bar-list-dependencies/issues/1

--- debug-bar-list-dependencies.php
+++ debug-bar-list-dependencies.php
@@ -38,7 +38,7 @@ function ps_listdeps_debug_bar_panels( $a ) {
 				
 				foreach ( $wp_scripts->do_items() as $loaded_scripts ) {
 
-					echo '<tr style="background-color:',  ( $i % 2 === 0 ) ? '#eee' : '#fff' , '"><td>', $i, '<td>', $loaded_scripts, '</td><td>', ( count( $wp_scripts->registered[$loaded_scripts]->deps ) > 0 ) ?  implode( " and ", $wp_scripts->registered[$loaded_scripts]->deps ) : '', '</td></tr>', "\n";
+					echo '<tr style="background-color:',  ( $i % 2 === 0 ) ? '#eee' : '#fff' , '"><td>', $i, '<td>', $loaded_scripts,'<br/>',$wp_scripts->registered[$loaded_scripts]->src, '</td><td>', ( count( $wp_scripts->registered[$loaded_scripts]->deps ) > 0 ) ?  join(' and ', array_filter(array_merge(array(join(', ', array_slice($wp_scripts->registered[$loaded_scripts]->deps, 0, -1))), array_slice($wp_scripts->registered[$loaded_scripts]->deps, -1)))) : '', '</td></tr>', "\n";
 					$i++;
 				}
 ?>
@@ -48,7 +48,7 @@ function ps_listdeps_debug_bar_panels( $a ) {
 
 				 $i = 1;
 				foreach ( $wp_styles->do_items() as $loaded_styles ) {
-					echo '<tr style="background-color:',  ( $i % 2 === 0 ) ? '#eee' : '#fff' , '"><td>', $i, '<td>', $loaded_styles, '</td><td>', ( count( $wp_styles->registered[$loaded_styles]->deps ) > 0 ) ?  implode( " and ", $wp_styles->registered[$loaded_styles]->deps ) : '', '</td></tr>', "\n";
+					echo '<tr style="background-color:',  ( $i % 2 === 0 ) ? '#eee' : '#fff' , '"><td>', $i, '<td>', $loaded_styles, '</td><td>', ( count( $wp_styles->registered[$loaded_styles]->deps ) > 0 ) ?  join(' and ', array_filter(array_merge(array(join(', ', array_slice($wp_styles->registered[$loaded_styles]->deps, 0, -1))), array_slice($wp_styles->registered[$loaded_styles]->deps, -1)))) : '', '</td></tr>', "\n";
 					$i++;
 				}
 ?>
@@ -64,3 +64,40 @@ function ps_listdeps_debug_bar_panels( $a ) {
 	return $a;
 }
 add_filter( 'debug_bar_panels', 'ps_listdeps_debug_bar_panels' );
+
+/*
+
+http://cdnjs.cloudflare.com/ajax/libs/$name/$version/$filename
+
+http://cdnjs.com/packages.json:
+
+"packages": [
+        {
+            "name": "1140",
+            "filename": "1140.css",
+            "version": "2.0",
+            "description": "The 1140 grid fits perfectly into a 1280 monitor. On smaller monitors it becomes fluid and adapts to the width of the browser.",
+            "homepage": "http://cssgrid.net/",
+            "keywords": [
+                "1140"
+            ],
+            "maintainers": [
+                {
+                    "name": "Andy Taylor",
+                    "web": "http://www.andytlr.com/"
+                }
+            ],
+            "repositories": [],
+            "assets": [
+                {
+                    "version": "2.0",
+                    "files": [
+                        "1140.css",
+                        "1140.min.css"
+                    ]
+                }
+            ]
+        }
+
+*/
+
