96e2585567d639f367ad7252441430b0b8d85fbd
jrfnl <github_nospam@adviesenzo.nl>
2013-09-27 00:49:37 +0200

    PHP:
    Fix: duplicate script listings
    Added: non-obtrusive script/style source line
    Small adjustments to the html to be inline with the general debug bar semantics and styling
    
    CSS:
    - Moved table row highlighting to CSS
    - Some other minor adjustments to compensate for the twentythirteen theme screwing up the table display on the front-end
    
    Readme:
    - Update changelog
    - Updated stable tag as otherwise (when added to the WP SVN) no upgrade will be offered

--- debug-bar-list-dependencies.php
+++ debug-bar-list-dependencies.php
@@ -27,28 +27,55 @@ function ps_listdeps_debug_bar_panels( $a ) {
 				global $wp_scripts, $wp_styles;
 
 ?>
-			<div class="xx">
-				<table width="100%" cellspacing="2" cellpadding="5" class="deps-table">
-					<tr><th colspan="3" style="font-size: 1.5rem;font-weight: bold">Enqueued Scripts</th></tr>
-					<tr><td>Order</td><td><b>Loaded</b></td><td><b>Dependencies</b></td></tr>
+			<div class="debug-bar-list-dependencies">
+				<h2>Enqueued Scripts</h2>
+				<table class="debug-bar-table deps-table">
+					<thead>
+						<tr><th>Order</th><th>Loaded</th><th>Dependencies</th></tr>
+					</thead>
+					<tbody>
 					<?php
-				$i = 1;				
-				foreach ( array_merge($wp_scripts->done, $wp_scripts->in_footer) as $loaded_scripts) {
+				$i = 1;
+				$loaded_scripts = array_merge($wp_scripts->done, $wp_scripts->in_footer);
+				$loaded_scripts = array_unique($loaded_scripts);
 
-					echo '<tr style="background-color:',  ( $i % 2 === 0 ) ? '#eee' : '#fff' , '"><td>', $i, '<td>', $loaded_scripts,  /*'<br/>',$wp_scripts->registered[$loaded_scripts]->src,*/ '</td><td>', ( count( $wp_scripts->registered[$loaded_scripts]->deps ) > 0 ) ?  join(' and ', array_filter(array_merge(array(join(', ', array_slice($wp_scripts->registered[$loaded_scripts]->deps, 0, -1))), array_slice($wp_scripts->registered[$loaded_scripts]->deps, -1)))) : '', '</td></tr>', "\n";
+				foreach ( $loaded_scripts as $loaded_script) {
+
+					echo '<tr><td>', $i, '<td>',
+						$loaded_script,
+						'</td><td>',
+						( count( $wp_scripts->registered[$loaded_script]->deps ) > 0 ) ?  join(' and ', array_filter(array_merge(array(join(', ', array_slice($wp_scripts->registered[$loaded_script]->deps, 0, -1))), array_slice($wp_scripts->registered[$loaded_script]->deps, -1)))) : '&nbsp;',
+						'</td></tr>',
+						'<tr class="src"><td>&nbsp;</td><td colspan="2">',
+						$wp_scripts->registered[$loaded_script]->src,
+						'</td></tr>', "\n";
 					$i++;
 				}
 ?>
-					<tr><th colspan="3" style="font-size: 1.5rem;font-weight: bold">Enqueued Styles</th></tr>
-					<tr><td>Order</td><td><b>Loaded</b></td><td><b>Dependencies</b></td></tr>
+					</tbody>
+  				</table>
+  				<h2>Enqueued Styles</h2>
+				<table class="debug-bar-table deps-table">
+					<thead>
+						<tr><th>Order</th><th>Loaded</th><th>Dependencies</th></tr>
+					</thead>
+					<tbody>
 					<?php
 
 				$i = 1;
 				foreach ( $wp_styles->done as $loaded_styles ) {
-					echo '<tr style="background-color:',  ( $i % 2 === 0 ) ? '#eee' : '#fff' , '"><td>', $i, '<td>', $loaded_styles, '</td><td>', ( count( $wp_styles->registered[$loaded_styles]->deps ) > 0 ) ?  join(' and ', array_filter(array_merge(array(join(', ', array_slice($wp_styles->registered[$loaded_styles]->deps, 0, -1))), array_slice($wp_styles->registered[$loaded_styles]->deps, -1)))) : '', '</td></tr>', "\n";
+					echo '<tr><td>', $i, '<td>',
+						$loaded_styles,
+						'</td><td>',
+						( count( $wp_styles->registered[$loaded_styles]->deps ) > 0 ) ?  join(' and ', array_filter(array_merge(array(join(', ', array_slice($wp_styles->registered[$loaded_styles]->deps, 0, -1))), array_slice($wp_styles->registered[$loaded_styles]->deps, -1)))) : '&nbsp;',
+						'</td></tr>',
+						'<tr class="src"><td>&nbsp;</td><td colspan="2">',
+						$wp_styles->registered[$loaded_styles]->src,
+						'</td></tr>', "\n";
 					$i++;
 				}
 ?>
+					</tbody>
 				</table>
 			</div>
 			<?php
