8c60a847df5ff1b4fbf100f63ce510c09d3bce44
jrfnl <github_nospam@adviesenzo.nl>
2016-04-12 09:03:23 +0200

    Add travis script & update plugin for WPCS standards.
    Travis script tasks:
    * linting of PHP on various versions
    * PHPCS check against WPCS standards
    
    Other significant changes related to WPCS/best practices:
    * Abstracted some duplicate code to their own methods.
    * Ensured that all methods have visibility added.
    * No nesting of classes within functions.
    * Use the proper hooks for enqueuing the styles.
    * Removed the switching between HTML and PHP in favour of echo statements as this makes complying with the standards easier & has a small performance benefit.
    * Very much simplified & made much more efficient the creation of the dependencies string.
    * Minor UX improvement to the dependencies string for readability.
    * Always use `function/class_exists()` wrappers.

--- debug-bar-list-dependencies.php
+++ debug-bar-list-dependencies.php
@@ -1,125 +1,203 @@
 <?php
-/*
-Plugin Name:  Debug Bar List Script & Style Dependencies
-Version:      1.0.6
-Plugin URI:   http://soderlind.no/debug-bar-list-script-and-style-dependencies/
-Description:  Debug Bar List Script & Style Dependencies is an add-on to WordPress Debug Bar
-Author:       Per Soderlind
-Author URI:   http://www.soderlind.no
-Depends:      Debug Bar
-Text Domain:  debug-bar-list-dependencies
-Domain Path:  /languages/
-*/
+/**
+ * Debug Bar List Script & Style Dependencies, a WordPress plugin.
+ *
+ * @package     WordPress\Plugins\Debug Bar List Dependencies
+ * @author      Per Soderlind
+ * @link        https://github.com/soderlind/debug-bar-list-dependencies
+ * @version     1.0.6
+ *
+ * @wordpress-plugin
+ * Plugin Name: Debug Bar List Script & Style Dependencies
+ * Plugin URI:  http://soderlind.no/debug-bar-list-script-and-style-dependencies/
+ * Description: Debug Bar List Script & Style Dependencies is an add-on to WordPress Debug Bar.
+ * Version:     1.0.6
+ * Author:      Per Soderlind
+ * Author URI:  http://www.soderlind.no/
+ * Depends:     Debug Bar
+ * Text Domain: debug-bar-list-dependencies
+ * Domain Path: /languages
+ */
 
+if ( class_exists( 'Debug_Bar_Panel' ) && ! class_exists( 'PS_Listdeps_Debug_Bar_Panel' ) ) {
 
-function ps_listdeps_debug_bar_panels( $a ) {
-	if ( class_exists( 'Debug_Bar_Panel' ) ) {
+	/**
+	 * Debug Bar List Dependencies Panel.
+	 */
+	class PS_Listdeps_Debug_Bar_Panel extends Debug_Bar_Panel {
 
-		class ps_listdeps_Debug_Bar_Panel extends Debug_Bar_Panel {
-			
-			const DBLD_NAME = 'debug-bar-list-deps';
+		const DBLD_NAME = 'debug-bar-list-deps';
 
-			const DBLD_STYLES_VERSION = '1.0.6';
+		const DBLD_STYLES_VERSION = '1.0.6';
 
-			function init() {
-				$this->enqueue();
-				$this->load_textdomain( 'debug-bar-list-dependencies' );
+		/**
+		 * Constructor.
+		 */
+		public function init() {
+			$this->title( __( 'Script & Style Dependencies', 'debug-bar-list-dependencies' ) );
+
+			$this->load_textdomain( 'debug-bar-list-dependencies' );
+
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+		}
+
+		/**
+		 * Load the plugin text			 *
+		 * Compatible with use of the plugin in the must-use plugins directory.
+		 *
+		 * @param string $domain Text domain to load.
+		 */
+		protected function load_textdomain( $domain ) {
+			if ( is_textdomain_loaded( $domain ) ) {
+				return;
 			}
 
-			/**
-			 * Load the plugin text strings.
-			 *
-			 * Compatible with use of the plugin in the must-use plugins directory.
-			 *
-			 * @param string $domain Text domain to load.
-			 */
-			protected function load_textdomain( $domain ) {
-				if ( is_textdomain_loaded( $domain ) ) {
-					return;
-				}
-
-				$lang_path = dirname( plugin_basename( __FILE__ ) ) . '/languages';
-				if ( false === strpos( __FILE__, basename( WPMU_PLUGIN_DIR ) ) ) {
-					load_plugin_textdomain( $domain, false, $lang_path );
-				} else {
-					load_muplugin_textdomain( $domain, $lang_path );
-				}
-			}
-
-			function enqueue() {
-				$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min' );
-				wp_enqueue_style( self::DBLD_NAME, plugins_url( 'css/debug-bar-list-deps' . $suffix . '.css', __FILE__ ), array( 'debug-bar' ), self::DBLD_STYLES_VERSION );
-			}
-
-			function render( ) {
-				global $wp_scripts, $wp_styles;
-				
-				$loaded_scripts = array_merge($wp_scripts->done, $wp_scripts->in_footer);
-				$loaded_scripts = array_unique($loaded_scripts);
-				$styles = $wp_styles->done;
-
-
-?>
-			<div class="debug-bar-list-dependencies">
-				<h2><span><?php echo esc_html__( 'Total Enqueued Scripts:', 'debug-bar-list-dependencies' ); ?></span><?php echo count( $loaded_scripts ); ?></h2>
-				<h2><span><?php echo esc_html__( 'Total Enqueued Styles:', 'debug-bar-list-dependencies' ); ?></span><?php echo count( $styles ); ?></h2>
-
-				<h3><?php echo esc_html__( 'Enqueued Scripts', 'debug-bar-list-dependencies' ); ?></h3>
-				<table class="debug-bar-table deps-table">
-					<thead>
-						<tr><th><?php echo esc_html__( 'Order', 'debug-bar-list-dependencies' ); ?></th><th><?php echo esc_html__( 'Loaded', 'debug-bar-list-dependencies' ); ?></th><th><?php echo esc_html__( 'Dependencies', 'debug-bar-list-dependencies' ); ?></th></tr>
-					</thead>
-					<tbody>
-					<?php
-				$i = 1;
-
-				foreach ( $loaded_scripts as $loaded_script) {
-
-					echo '<tr><td>', $i, '<td>',
-						$loaded_script,
-						'</td><td>',
-						( count( $wp_scripts->registered[$loaded_script]->deps ) > 0 ) ?  join( ' ' . __( 'and', 'debug-bar-list-dependencies' ) . ' ', array_filter(array_merge(array(join(', ', array_slice($wp_scripts->registered[$loaded_script]->deps, 0, -1))), array_slice($wp_scripts->registered[$loaded_script]->deps, -1)))) : '&nbsp;',
-						'</td></tr>',
-						'<tr class="src"><td>&nbsp;</td><td colspan="2">',
-						esc_html($wp_scripts->registered[$loaded_script]->src),
-						'</td></tr>', "\n";
-					$i++;
-				}
-?>
-					</tbody>
-  				</table>
-  				<h3><?php echo esc_html__( 'Enqueued Styles', 'debug-bar-list-dependencies' ); ?></h3>
-				<table class="debug-bar-table deps-table">
-					<thead>
-						<tr><th><?php echo esc_html__( 'Order', 'debug-bar-list-dependencies' ); ?></th><th><?php echo esc_html__( 'Loaded', 'debug-bar-list-dependencies' ); ?></th><th><?php echo esc_html__( 'Dependencies', 'debug-bar-list-dependencies' ); ?></th></tr>
-					</thead>
-					<tbody>
-					<?php
-
-				$i = 1;
-				foreach ( $styles as $loaded_styles ) {
-					echo '<tr><td>', $i, '<td>',
-						$loaded_styles,
-						'</td><td>',
-						( count( $wp_styles->registered[$loaded_styles]->deps ) > 0 ) ?  join( ' ' . __( 'and', 'debug-bar-list-dependencies' ) . ' ', array_filter(array_merge(array(join(', ', array_slice($wp_styles->registered[$loaded_styles]->deps, 0, -1))), array_slice($wp_styles->registered[$loaded_styles]->deps, -1)))) : '&nbsp;',
-						'</td></tr>',
-						'<tr class="src"><td>&nbsp;</td><td colspan="2">',
-						esc_html($wp_styles->registered[$loaded_styles]->src),
-						'</td></tr>', "\n";
-					$i++;
-				}
-?>
-					</tbody>
-				</table>
-			</div>
-			<?php
+			$lang_path = dirname( plugin_basename( __FILE__ ) ) . '/languages';
+			if ( false === strpos( __FILE__, basename( WPMU_PLUGIN_DIR ) ) ) {
+				load_plugin_textdomain( $domain, false, $lang_path );
+			} else {
+				load_muplugin_textdomain( $domain, $lang_path );
 			}
 		}
 
+		/**
+		 * Enqueue the css file.
+		 */
+		public function enqueue() {
+			$suffix = ( ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min' );
+			wp_enqueue_style( self::DBLD_NAME, plugins_url( 'css/debug-bar-list-deps' . $suffix . '.css', __FILE__ ), array( 'debug-bar' ), self::DBLD_STYLES_VERSION );
+		}
 
-		$a[]=new ps_listdeps_Debug_Bar_Panel( __( 'Script & Style Dependencies', 'debug-bar-list-dependencies' ) );
+		/**
+		 * Render the output.
+		 */
+		public function render() {
+			global $wp_scripts, $wp_styles;
+
+			$loaded_scripts = array_merge( $wp_scripts->done, $wp_scripts->in_footer );
+			$loaded_scripts = array_unique( $loaded_scripts );
+			$styles         = $wp_styles->done;
+
+			echo '
+		<div class="debug-bar-list-dependencies">
+			<h2><span>',
+			esc_html__( 'Total Enqueued Scripts:', 'debug-bar-list-dependencies' ),
+			'</span>', count( $loaded_scripts ), '</h2>
+			<h2><span>',
+			esc_html__( 'Total Enqueued Styles:', 'debug-bar-list-dependencies' ),
+			'</span>', count( $styles ), '</h2>';
+
+			$i = 1;
+			$this->render_table_header( __( 'Enqueued Scripts', 'debug-bar-list-dependencies' ) );
+			foreach ( $loaded_scripts as $loaded_script ) {
+				$this->render_table_row(
+					$i,
+					$loaded_script,
+					$wp_scripts->registered[ $loaded_script ]->deps,
+					$wp_scripts->registered[ $loaded_script ]->src
+				);
+				$i++;
+			}
+			$this->render_table_close();
+
+			$i = 1;
+			$this->render_table_header( __( 'Enqueued Styles', 'debug-bar-list-dependencies' ) );
+			foreach ( $styles as $loaded_styles ) {
+				$this->render_table_row(
+					$i,
+					$loaded_styles,
+					$wp_styles->registered[ $loaded_styles ]->deps,
+					$wp_styles->registered[ $loaded_styles ]->src
+				);
+				$i++;
+			}
+			$this->render_table_close();
+
+			echo '
+		</div>';
+		}
+
+		/**
+		 * Render the table header.
+		 *
+		 * @param string $title Panel title.
+		 */
+		private function render_table_header( $title ) {
+			echo '
+  				<h3>', esc_html( $title ), '</h3>
+			<table class="debug-bar-table deps-table">
+				<thead>
+					<tr><th>',
+					esc_html__( 'Order', 'debug-bar-list-dependencies' ),
+					'</th><th>',
+					esc_html__( 'Loaded', 'debug-bar-list-dependencies' ),
+					'</th><th>',
+					esc_html__( 'Dependencies', 'debug-bar-list-dependencies' ),
+					'</th></tr>
+				</thead>
+				<tbody>';
+		}
+
+		/**
+		 * Render a table row.
+		 *
+		 * @param int    $row_nr       The row number.
+		 * @param string $item         The current item to render.
+		 * @param array  $dependencies The dependencies for the current item.
+		 * @param string $src          The source url for the current item.
+		 */
+		private function render_table_row( $row_nr, $item, $dependencies, $src ) {
+			echo '<tr><td>', (int) $row_nr, '</td><td>',
+				esc_html( $item ),
+				'</td><td>';
+
+			if ( ! empty( $dependencies ) && is_array( $dependencies ) ) {
+				$dependency_string = '';
+
+				// Remove the last item from $dependencies & remember it.
+				$last = array_pop( $dependencies );
+
+				if ( ! empty( $dependencies ) ) {
+					$dependency_string = esc_html( implode( ', ', $dependencies ) ) .
+						' <span class="glue">' . esc_html__( 'and', 'debug-bar-list-dependencies' ) . '</span> ';
+				}
+
+				echo $dependency_string, esc_html( $last ); // WPCS: XSS ok.
+			} else {
+				echo '&nbsp;';
+			}
+
+			echo '</td></tr>',
+				'<tr class="src"><td>&nbsp;</td><td colspan="2">',
+				esc_html( $src ),
+				'</td></tr>', "\n";
+		}
+
+		/**
+		 * Render the table closing.
+		 */
+		private function render_table_close() {
+			echo '
+				</tbody>
+  				</table>';
+		}
 	}
-	return $a;
 }
-add_filter( 'debug_bar_panels', 'ps_listdeps_debug_bar_panels' );
-?>
+
+
+if ( ! function_exists( 'ps_listdeps_debug_bar_panels' ) ) {
+	/**
+	 * Add a Scripts & Styles dependencies panel to the Debug Bar.
+	 *
+	 * @param array $panels Existing debug bar panels.
+	 *
+	 * @return array
+	 */
+	function ps_listdeps_debug_bar_panels( $panels ) {
+		$panels[] = new PS_Listdeps_Debug_Bar_Panel();
+		return $panels;
+	}
+
+	add_filter( 'debug_bar_panels', 'ps_listdeps_debug_bar_panels' );
+}
